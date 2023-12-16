63cadce7aeebf7e203de09786e2da3e083885be5
Divyanshu Gautam <57661791+ObitoDevv@users.noreply.github.com>
2023-12-16 18:40:24 +0530

    Delete COOK_BOX-main/COOK_BOX-main directory

--- COOK_BOX-main/COOK_BOX-main/backend/HttpError.js
+++
@@ -1,9 +0,0 @@
-class HttpError extends Error{
-
-    constructor(message,errorCode){
-        super(message); // add a 'message' property
-        this.code= errorCode;// add a "code" perperty
-
-    }
-}
-module.exports=HttpError
--- COOK_BOX-main/COOK_BOX-main/frontend/src/user/pages/User.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const User = () => {
-  return <header > HERE AT USER</header>;
-};
-
-export default User;
